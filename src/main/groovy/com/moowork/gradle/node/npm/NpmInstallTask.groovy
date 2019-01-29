4d934b1630b27a63d66739509c556b3a1b5fdd1a
Björn Magnusson <bjornmagnusson1984@gmail.com>
2019-01-29 22:07:48 +0100

    Added gradle build cache support for npm install

--- src/main/groovy/com/moowork/gradle/node/npm/NpmInstallTask.groovy
+++ src/main/groovy/com/moowork/gradle/node/npm/NpmInstallTask.groovy
@@ -1,4 +1,10 @@
 package com.moowork.gradle.node.npm
+import com.moowork.gradle.node.NodeExtension
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 
 import com.moowork.gradle.node.NodeExtension
 
@@ -19,7 +25,32 @@ class NpmInstallTask
             def ext = this.project.extensions.getByType(NodeExtension)
             setNpmCommand( ext.getNpmInstallCommand() )
             getInputs().file( new File( (File) this.project.node.nodeModulesDir, 'package.json' ) )
+            getInputs().file( new File( (File) this.project.node.nodeModulesDir, 'package-lock.json'))
             getOutputs().dir( new File( (File) this.project.node.nodeModulesDir, 'node_modules' ) )
         }
+    }
+
+    @InputFile
+    @Optional
+    @PathSensitive(PathSensitivity.RELATIVE)
+    protected getPackageJsonFile()
+    {
+        def packageJsonFile = new File(this.project.extensions.getByType(NodeExtension).nodeModulesDir, 'package.json')
+        return packageJsonFile.exists() ? packageJsonFile : null
+    }
+
+    @InputFile
+    @Optional
+    @PathSensitive(PathSensitivity.RELATIVE)
+    protected getPackageLockFile()
+    {
+        def lockFile = new File(this.project.extensions.getByType(NodeExtension).nodeModulesDir, 'package-lock.json')
+        return lockFile.exists() ? lockFile : null
+    }
+
+    @OutputDirectory
+    protected getNodeModulesDir()
+    {
+        return new File(this.project.extensions.getByType(NodeExtension).nodeModulesDir, 'node_modules')
     }
 }
