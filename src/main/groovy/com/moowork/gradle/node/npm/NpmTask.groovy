ef5d7458753c8df73395bd608187ef8e4087e291
Alex Nordlund <deep.alexander@gmail.com>
2019-09-16 19:22:50 +0200

    Make both npmCommand and args in NpmTask optional again.
    Previously both were optional, even though you really at least one of them.

--- src/main/groovy/com/moowork/gradle/node/npm/NpmTask.groovy
+++ src/main/groovy/com/moowork/gradle/node/npm/NpmTask.groovy
@@ -4,6 +4,7 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Nested
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 import org.gradle.process.ExecResult
 
@@ -46,6 +47,7 @@ class NpmTask
     }
 
     @Input
+    @Optional
     String[] getNpmCommand() {
         return npmCommand
     }
@@ -56,6 +58,7 @@ class NpmTask
     }
 
     @Input
+    @Optional
     List<?> getArgs()
     {
         return this.args
