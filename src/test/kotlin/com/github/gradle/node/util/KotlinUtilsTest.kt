b8746b227ab2d1f3bf7fe8b079899011c8ef3750
Benoit Sautel <benoit.sautel@gmail.com>
2020-04-05 16:14:11 +0200

    Use JUnit 5 in non spock tests. Spock will use JUnit 5 but is not available yet.

--- src/test/kotlin/com/github/gradle/node/util/KotlinUtilsTest.kt
+++ src/test/kotlin/com/github/gradle/node/util/KotlinUtilsTest.kt
@@ -1,7 +1,7 @@
 package com.github.gradle.node.util
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 class KotlinUtilsTest {
     @Test
