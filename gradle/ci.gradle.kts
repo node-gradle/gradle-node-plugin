0b9dd8fa175662addded97bbeb2daabed9c6fe7d
Benoit Sautel <benoit.sautel@gmail.com>
2020-04-21 09:27:36 +0200

    Simplify the build. Remove old publication configuration that seems to be no longer used (artifactory publication). Simply use the Gradle plugin publish plugin to publish it to Gradle portal.

--- gradle/ci.gradle.kts
+++
@@ -1,7 +0,0 @@
-extra.properties["isMaster"] = System.getenv()["TRAVIS_BRANCH"] == "master"
-
-tasks.register("ci") {
-    dependsOn("build")
-    description = "Continuous integration tasks"
-    group = "Build"
-}
