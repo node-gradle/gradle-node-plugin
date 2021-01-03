61f9d4c4649b69cb3570701122b7b4bfb931ff46
Alex Nordlund <deep.alexander@gmail.com>
2021-01-03 20:18:32 +0100

    Add tests for PlatformHelper arm functionality.

--- src/main/kotlin/com/github/gradle/node/util/PlatformHelper.kt
+++ src/main/kotlin/com/github/gradle/node/util/PlatformHelper.kt
@@ -22,7 +22,7 @@ open class PlatformHelper constructor(private val props: Properties = System.get
              * As Java just returns "arm" on all ARM variants, we need a system call to determine the exact arch. Unfortunately some JVMs say aarch32/64, so we need an additional
              * conditional. Additionally, the node binaries for 'armv8l' are called 'arm64', so we need to distinguish here.
              */
-            arch == "arm" || arch.startsWith("aarch") -> execute("uname", "-m").mapIf({ it == "armv8l" }) { "arm64" }
+            arch == "arm" || arch.startsWith("aarch") -> property("uname").mapIf({ it == "armv8l" }) { "arm64" }
             arch.contains("64") -> "x64"
             else -> "x86"
         }
@@ -32,7 +32,10 @@ open class PlatformHelper constructor(private val props: Properties = System.get
 
     private fun property(name: String): String {
         val value = props.getProperty(name)
-        return value ?: System.getProperty(name) ?: error("Unable to find a value for property [$name].")
+        return value ?: System.getProperty(name) ?:
+            // Added so that we can test osArch on Windows and on non-arm systems
+            if (name == "uname") execute("uname", "-m")
+            else error("Unable to find a value for property [$name].")
     }
 
     companion object {
