2fc3cda998b9958da8c7d8003c7b1b4fec0bd05f
Alex Nordlund <deep.alexander@gmail.com>
2021-01-09 16:28:20 +0100

    Add Javadoc to NodeExtension

--- src/main/kotlin/com/github/gradle/node/NodeExtension.kt
+++ src/main/kotlin/com/github/gradle/node/NodeExtension.kt
@@ -7,19 +7,82 @@ import org.gradle.kotlin.dsl.property
 
 open class NodeExtension(project: Project) {
     private val cacheDir = project.layout.projectDirectory.dir(".gradle")
+
+    /**
+     * The directory where Node.js is unpacked (when download is true)
+     */
     val workDir = project.objects.directoryProperty().convention(cacheDir.dir("nodejs"))
+
+    /**
+     * The directory where npm is installed (when a specific version is defined)
+     */
     val npmWorkDir = project.objects.directoryProperty().convention(cacheDir.dir("npm"))
+
+    /**
+     * The directory where yarn is installed (when a Yarn task is used)
+     */
     val yarnWorkDir = project.objects.directoryProperty().convention(cacheDir.dir("yarn"))
+
+    /**
+     * The Node.js project directory location
+     * This is where the package.json file and node_modules directory are located
+     * By default it is at the root of the current project
+     */
     val nodeProjectDir = project.objects.directoryProperty().convention(project.layout.projectDirectory)
+
+    /**
+     * Version of node to download and install (only used if download is true)
+     * It will be unpacked in the workDir
+     */
     val version = project.objects.property<String>().convention(DEFAULT_NODE_VERSION)
+
+    /**
+     * Version of npm to use
+     * If specified, installs it in the npmWorkDir
+     * If empty, the plugin will use the npm command bundled with Node.js
+     */
     val npmVersion = project.objects.property<String>().convention("")
+
+    /**
+     * Version of Yarn to use
+     * Any Yarn task first installs Yarn in the yarnWorkDir
+     * It uses the specified version if defined and the latest version otherwise (by default)
+     */
     val yarnVersion = project.objects.property<String>().convention("")
+
+    /**
+     * Base URL for fetching node distributions
+     * Only used if download is true
+     * Change it if you want to use a mirror
+     * Or set to null if you want to add the repository on your own.
+     */
     val distBaseUrl = project.objects.property<String>()
+
     val npmCommand = project.objects.property<String>().convention("npm")
     val npxCommand = project.objects.property<String>().convention("npx")
-    val npmInstallCommand = project.objects.property<String>().convention("install")
     val yarnCommand = project.objects.property<String>().convention("yarn")
+
+    /**
+     * The npm command executed by the npmInstall task
+     * By default it is install but it can be changed to ci
+     */
+    val npmInstallCommand = project.objects.property<String>().convention("install")
+
+    /**
+     * Whether to download and install a specific Node.js version or not
+     * If false, it will use the globally installed Node.js
+     * If true, it will download node using above parameters
+     * Note that npm is bundled with Node.js
+     */
     val download = project.objects.property<Boolean>().convention(false)
+
+    /**
+     * Whether the plugin automatically should add the proxy configuration to npm and yarn commands
+     * according the proxy configuration defined for Gradle
+     *
+     * Disable this option if you want to configure the proxy for npm or yarn on your own
+     * (in the .npmrc file for instance)
+     */
     val useGradleProxySettings = project.objects.property<Boolean>().convention(true)
 
     @Suppress("unused")
@@ -29,6 +92,7 @@ open class NodeExtension(project: Project) {
     init {
         distBaseUrl.set("https://nodejs.org/dist")
     }
+
 
     companion object {
         const val NAME = "node"
