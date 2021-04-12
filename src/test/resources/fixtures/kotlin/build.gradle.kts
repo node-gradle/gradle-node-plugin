6d30c11a54c4daeef3b6c20b50ea6aa18efc54b7
markslater <mark.slater@mail.com>
2021-04-12 20:24:33 +0100

    Support allowInsecureProtocol for downloading Node.js over HTTP.

--- src/test/resources/fixtures/kotlin/build.gradle.kts
+++ src/test/resources/fixtures/kotlin/build.gradle.kts
@@ -21,7 +21,7 @@ node {
     yarnVersion.set("")
     npmInstallCommand.set("install")
     distBaseUrl.set("https://nodejs.org/dist")
-    allowInsecureProtocol.set(null)
+    allowInsecureProtocol.set(null as String?)
     download.set(false)
     workDir.set(file("${project.projectDir}/.cache/nodejs"))
     npmWorkDir.set(file("${project.projectDir}/.cache/npm"))
