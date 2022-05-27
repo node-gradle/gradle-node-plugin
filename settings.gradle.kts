67de2ba04346152e05ce1e5a85dcbb7565d46292
Alex Nordlund <deep.alexander@gmail.com>
2022-05-27 11:01:59 +0200

    Publish Build Scans from CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
+plugins {
+    id("com.gradle.enterprise") version("3.10.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        if (System.getenv().containsKey("CI")) {
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
+
 rootProject.name = "gradle-node-plugin"
