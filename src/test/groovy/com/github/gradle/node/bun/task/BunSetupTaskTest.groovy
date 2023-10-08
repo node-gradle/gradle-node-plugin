e9694257d47ee7b24189ffa8c6778dac49355c6c
Harrel <marcin.slowik95@gmail.com>
2023-10-08 22:25:30 +0200

    add bunSetup unit test

---
+++ src/test/groovy/com/github/gradle/node/bun/task/BunSetupTaskTest.groovy
@@ -0,0 +1,47 @@
+package com.github.gradle.node.bun.task
+
+import com.github.gradle.node.task.AbstractTaskTest
+
+class BunSetupTaskTest
+    extends AbstractTaskTest
+{
+    def "exec bunSetup task without any bun version specified"() {
+        given:
+        def task = project.tasks.create('simple', BunSetupTask)
+        mockProjectApiHelperExec(task)
+
+        when:
+        project.evaluate()
+        task.exec()
+
+        then:
+        1 * execSpec.setArgs({ args ->
+            def expectedBunInstallPath = projectDir.toPath().resolve('.gradle').resolve('bun')
+                    .resolve('bun-latest').toAbsolutePath().toString()
+            def expectedArgs = ['install', '--global', '--no-save', '--prefix', expectedBunInstallPath, 'bun']
+            // Workaround a strange issue on Github actions macOS hosts
+            return args.collect { it.replace("^/private/", "/") } == expectedArgs
+        })
+    }
+
+    def "exec bunSetup task with bun version specified"() {
+        given:
+        def bunVersion = '1.0.0'
+        nodeExtension.bunVersion.set(bunVersion)
+        def task = project.tasks.create('simple', BunSetupTask)
+        mockProjectApiHelperExec(task)
+
+        when:
+        project.evaluate()
+        task.exec()
+
+        then:
+        1 * execSpec.setArgs({ args ->
+            def expectedBunInstallPath = projectDir.toPath().resolve('.gradle').resolve('bun')
+                    .resolve("pnpm-v${bunVersion}").toAbsolutePath().toString()
+            def expectedArgs = ['install', '--global', '--no-save', '--prefix', expectedBunInstallPath, "bun@${bunVersion}"]
+            // Workaround a strange issue on Github actions macOS hosts
+            return args.collect { it.replace("^/private/", "/") } == expectedArgs
+        })
+    }
+}
