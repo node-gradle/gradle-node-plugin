a392a39753139a9003f8f13218d26b9c7c4ecfb5
Sadiq Kassamali <sadiqkassamali@gmail.com>
2024-07-31 02:46:32 -0500

    Bump com.fasterxml.jackson.core/jackson-databind to 2.17.2
    Bump com.fasterxml.jackson.core/jackson-databind to 2.17.2
    due to v3 https://security.snyk.io/vuln/SNYK-JAVA-COMFASTERXMLJACKSONCORE-7569538

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
